93a0df37e87a5695d9a468291d786ee217c36d67
FacuTello <facundostello@hotmail.com>
2023-02-23 17:48:30 -0300

    oneRound function done

--- main.js
+++ main.js
@@ -1,12 +1,31 @@
+/* ```js
+[Put your JavaScript Code here!]
+```*/
+
+
 
 function getComputerChoice(){
-    computerChoice = (Math.floor(Math.random() * 3));
+    let computerChoice = (Math.floor(Math.random() * 3));
     if (computerChoice === 0){
-        console.log("piedra");
+        return "piedra";
     } else if ( computerChoice === 1){
-        console.log("papel");
+        return "papel";
     } else{
-        console.log("tijeras");
+        return "tijeras";
     }
+    
 }
+let computerSelection = getComputerChoice();
+function oneRound(playerSelection , computerSelection){
+    
+    if (playerSelection == "piedra" && computerSelection == "piedra"){
+    console.log("ambos eligieron priedra. Es un empate");
+   }else if (playerSelection == "piedra" && computerSelection == "papel"){
+     console.log("Perdiste. El papel le gana a la piedra");
+   }else if(playerSelection == "piedra" && computerSelection == "tijeras"){
+     console.log("Ganaste. La piedra le gana a las tijeras");
+   }  
+} 
+
+
 
