9685f3dffd8a9a3d67bafa8c6cba373280ae1b3f
FacuTello <facundostello@hotmail.com>
2023-02-25 14:19:37 -0300

    Game done

--- main.js
+++ main.js
@@ -1,31 +1,39 @@
-/* ```js
-[Put your JavaScript Code here!]
-```*/
+function getComputerChoice() {
+	const choices = ["piedra", "papel", "tijera"];
+	const randomIndex = Math.floor(Math.random() * choices.length);
+	return choices[randomIndex];
+};
 
+let playerScore = 0;
+let computerScore = 0;
 
+function oneRound(playerSelection, computerSelection) {
+	playerSelection = playerSelection.toLowerCase();
+	if (playerSelection === "piedra" && computerSelection === "tijera" || playerSelection === "papel" && computerSelection === "piedra" ||
+		playerSelection === "tijera" && computerSelection === "papel") {
+		console.log("Ganaste! Elegiste " + playerSelection + " y tu oponente " + computerSelection);
+		playerScore += 1;
+	} else if (playerSelection === "piedra" && computerSelection === "papel" || playerSelection === "papel" && computerSelection === "tijera" ||
+		playerSelection === "tijera" && computerSelection === "piedra") {
+		console.log("Uhh! Perdiste. Tu oponente eligio " + computerSelection + " y tu " + playerSelection);
+		computerScore += 1; 
+	} else {
+		console.log("Empataron! Ambos eligieron " + playerSelection);
+	}
+};
 
-function getComputerChoice(){
-    let computerChoice = (Math.floor(Math.random() * 3));
-    if (computerChoice === 0){
-        return "piedra";
-    } else if ( computerChoice === 1){
-        return "papel";
-    } else{
-        return "tijeras";
-    }
-    
-}
-let computerSelection = getComputerChoice();
-function oneRound(playerSelection , computerSelection){
-    
-    if (playerSelection == "piedra" && computerSelection == "piedra"){
-    console.log("ambos eligieron priedra. Es un empate");
-   }else if (playerSelection == "piedra" && computerSelection == "papel"){
-     console.log("Perdiste. El papel le gana a la piedra");
-   }else if(playerSelection == "piedra" && computerSelection == "tijeras"){
-     console.log("Ganaste. La piedra le gana a las tijeras");
-   }  
-} 
+function game() {
+	for (let i = 0; i < 5; i++) {
+		playerSelection = prompt("Eliges Piedra, Papel, o Tijera?");
+		computerSelection = getComputerChoice();
+		oneRound(playerSelection, computerSelection);
+	}
 
-
-
+	if (playerScore > computerScore) {
+		console.log("Muy bien! Ganaste " + playerScore + " a " + computerScore);
+	} else if (computerScore > playerScore) {
+		console.log("Uhh! Perdiste " + computerScore + " a " + playerScore);
+	} else {
+		console.log("Empataron " + playerScore + " a " + computerScore + " intentalo otra vez!");
+	}
+};
