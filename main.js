f8504b3d03f02c3ee57baeb45cd747b3f3e58565
FacuTello <facundostello@hotmail.com>
2023-03-10 16:26:13 -0300

    Mobile version done

--- main.js
+++ main.js
@@ -4,66 +4,90 @@ function getComputerChoice() {
 	return choices[randomIndex];
 };
 
-let playerScore = 0;
-let computerScore = 0;
-
+	let playerScore = 0;
+	let computerScore = 0;
 function oneRound(playerSelection, computerSelection) {
+	
+ 
 	computerSelection = getComputerChoice();
 	if (playerSelection === "piedra" && computerSelection === "tijera" || playerSelection === "papel" && computerSelection === "piedra" ||
 		playerSelection === "tijera" && computerSelection === "papel") {
-		resultado.innerText = "Ganaste! Elegiste " + playerSelection + " y tu oponente " + computerSelection;
-		playerScore += 1;
+			resultado.innerText = "Ganaste! Elegiste " + playerSelection + " y tu oponente " + computerSelection;
+			playerScore += 1;
+		    puntaje.innerText = "El partido va " + playerScore + " a " + computerScore;
+			resultadoFinal.innerText = "";
 	} else if (playerSelection === "piedra" && computerSelection === "papel" || playerSelection === "papel" && computerSelection === "tijera" ||
 		playerSelection === "tijera" && computerSelection === "piedra") {
-		resultado.innerText = "Uhh! Perdiste. Tu oponente eligio " + computerSelection + " y tu " + playerSelection;
-		computerScore += 1; 
+			resultado.innerText = "Uhh! Perdiste. Tu oponente eligio " + computerSelection + " y tu " + playerSelection;
+			computerScore += 1; 
+            puntaje.innerText = "El partido va " + playerScore + " a " + computerScore;
+			resultadoFinal.innerText = "";
 	} else {
 		resultado.innerText = "Empataron! Ambos eligieron " + playerSelection;
-	}
+		puntaje.innerText = "El partido va " + playerScore + " a " + computerScore;
+		resultadoFinal.innerText = "";
+	
 };
-
-function game() {
-	for (let i = 0; i < 5; i++) {
-		playerSelection = prompt("Eliges Piedra, Papel, o Tijera?");
-		computerSelection = getComputerChoice();
-		oneRound(playerSelection, computerSelection);
-	}
-
-	if (playerScore > computerScore) {
-		console.log("Muy bien! Ganaste " + playerScore + " a " + computerScore);
-	} else if (computerScore > playerScore) {
-		console.log("Uhh! Perdiste " + computerScore + " a " + playerScore);
-	} else {
-		console.log("Empataron " + playerScore + " a " + computerScore + " intentalo otra vez!");
-	}
+       if (playerScore === 5){
+		resultadoFinal.innerText = "Muy bien Ganaste!!, intentalo otra vez!"; 
+		playerScore = 0;
+		computerScore = 0;
+		
+	   }else if (computerScore === 5) {
+		resultadoFinal.innerText = "Perdiste!, intentalo otra vez!";
+		playerScore = 0;
+		computerScore = 0;
+		};
 };
 
 const piedra = document.getElementById('piedra');
 const papel = document.getElementById('papel');
 const tijera = document.getElementById('tijera');
 
+ 
+piedra.addEventListener('click', clickPiedra);
+papel.addEventListener('click', clickPapel);
+tijera.addEventListener('click', clickTijera);
+
 function clickPiedra() {
-	oneRound("piedra", getComputerChoice());
-  }
+oneRound("piedra", getComputerChoice());
+  };
   
   function clickPapel() {
 	oneRound("papel", getComputerChoice());
-  }
+  };
   
   function clickTijera() {
 	oneRound("tijera", getComputerChoice());
-  }
+  };
+
   
   piedra.addEventListener('click', clickPiedra);
   papel.addEventListener('click', clickPapel);
   tijera.addEventListener('click', clickTijera);
 
-  const div = document.getElementById('ronda');
+  const divUnaRonda = document.getElementById('ronda');
   const resultado = document.createElement('p');
-  div.appendChild(resultado);
-  resultado.style.fontFamily = "Climate Crisis";
+  divUnaRonda.appendChild(resultado);
+  resultado.style.fontFamily = "Space Mono";
   resultado.style.textAlign = "center";
   resultado.style.marginTop = "40px";
+
+  const divJuego = document.getElementById('juego');
+  const puntaje = document.createElement('p');
+  divJuego.appendChild(puntaje);
+  puntaje.style.fontFamily = "Space Mono";
+  puntaje.style.textAlign = "center";
+  puntaje.style.marginTop = "40px";
+  const divResultado = document.getElementById('resultado');
+  let resultadoFinal = document.createElement('p');
+  divResultado.appendChild(resultadoFinal);
+  resultadoFinal.style.fontFamily = "Space Mono";
+  resultadoFinal.style.textAlign = "center";
+  resultadoFinal.style.marginTop = "40px";
+
+
+
 
   
 
